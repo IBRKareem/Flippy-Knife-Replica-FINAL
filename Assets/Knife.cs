8c9d9ebdefb23e57afcda65128d33a0994650991
IBRKareem <36110628+IBRKareem@users.noreply.github.com>
2018-07-18 14:38:40 -0400

    Added Bugged Knife Movement
    Knife flies off screen completely.

---
+++ Assets/Knife.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	private Vector2 startSwipe;
+	private Vector2 endSwipe;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			startSwipe = Input.mousePosition;
+		}
+		if (Input.GetMouseButtonUp (0))
+		{
+			endSwipe = Input.mousePosition;
+			Swipe ();
+		}
+	}
+
+	void Swipe ()
+	{
+		Vector2 swipe = endSwipe - startSwipe;
+
+		rb.AddForce (swipe, ForceMode.Impulse);
+
+	}
+}
